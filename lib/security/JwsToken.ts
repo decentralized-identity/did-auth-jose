4f675a16ea3ce81365ae6758dc89b62458ef2423
Logan Girvin <logirvin@microsoft.com>
2019-03-08 17:02:56 -0800

    starting work on trying to get JSON Serialization supported.

--- lib/security/JwsToken.ts
+++ lib/security/JwsToken.ts
@@ -1,7 +1,7 @@
 import Base64Url from '../utilities/Base64Url';
 import JoseToken from './JoseToken';
 import PublicKey from '../security/PublicKey';
-import { PrivateKey } from '..';
+import { PrivateKey, CryptoFactory } from '..';
 
 /**
  * Definition for a delegate that can verfiy signed data.
@@ -13,6 +13,45 @@ type VerifySignatureDelegate = (signedContent: string, signature: string, jwk: P
  * This class hides the JOSE and crypto library dependencies to allow support for additional crypto algorithms.
  */
 export default class JwsToken extends JoseToken {
+
+  // used for verification if a JSON Serialized JWS was given
+  private readonly protected: string | undefined;
+  private header: {[key: string]: any} | undefined;
+  private readonly signature: string | undefined;
+  private readonly isFlattenedJSONSerialized: boolean;
+
+  constructor(content: string | object, protected cryptoFactory: CryptoFactory) {
+    super(content, cryptoFactory);
+    this.isFlattenedJSONSerialized = false;
+    // Check for JSON Serialization and reparse content if appropriate
+    if (typeof content === 'object') {
+      const jsonObject: any = content;
+      if ('payload' in jsonObject && typeof jsonObject.payload === 'string') {
+        // TODO: General JWS JSON Serialization. For now check for signature and one of protected or header
+        
+        if ('signature' in jsonObject && typeof jsonObject.signature === 'string') {
+          this.signature = jsonObject.signature;
+          // Flattened JWS JSON Serialization
+          if (!('protected' in jsonObject && typeof jsonObject.protected === 'string') && 
+            !('header' in jsonObject && typeof jsonObject.header === 'object')) {
+            // invalid JWS JSON Serialization
+            return;
+          }
+          if ('protected' in jsonObject && typeof jsonObject.protected === 'string') {
+            this.protected = jsonObject.protected;
+          }
+          if ('header' in jsonObject && typeof jsonObject.header === 'object') {
+            this.header = jsonObject.header;
+          }
+          // if we've gotten this far, we succeeded can can safely reset the content
+          this.content = jsonObject.payload;
+          this.isFlattenedJSONSerialized = true;
+          return;
+        }
+      }
+    }
+  }
+
   /**
    * Sign the given content using the given private key in JWK format.
    *
@@ -71,10 +110,34 @@ export default class JwsToken extends JoseToken {
     return verifiedData;
   }
 
+  
+  /**
+   * Gets the header as a JS object.
+   */
+  public getHeader (): any {
+    if (this.isFlattenedJSONSerialized) {
+      let headers = this.header;
+      if (!headers) {
+        headers = {};
+      }
+      if (this.protected) {
+        const jsonString = Base64Url.decode(this.protected);
+    
+        const protect = JSON.parse(jsonString) as {[key: string]: any};
+        headers = Object.assign(headers, protect);
+      }
+      return headers;
+    }
+    return super.getHeader();
+  }
+
   /**
    * Gets the signed content (i.e. '<header>.<payload>').
    */
   private getSignedContent (): string {
+    if (this.isFlattenedJSONSerialized) {
+      return this.protected! + '.' + this.content;
+    }
     const signedContentLength = this.content.lastIndexOf('.');
     const signedContent = this.content.substr(0, signedContentLength);
 
@@ -85,6 +148,9 @@ export default class JwsToken extends JoseToken {
    * Gets the base64 URL decrypted payload.
    */
   public getPayload (): any {
+    if (this.isFlattenedJSONSerialized) {
+      return this.content;
+    }
     const payloadStartIndex = this.content.indexOf('.') + 1;
     const payloadExclusiveEndIndex = this.content.lastIndexOf('.');
     const payload = this.content.substring(payloadStartIndex, payloadExclusiveEndIndex);
@@ -96,6 +162,9 @@ export default class JwsToken extends JoseToken {
    * Gets the signature string.
    */
   private getSignature (): string {
+    if (this.isFlattenedJSONSerialized) {
+      return this.signature!;
+    }
     const signatureStartIndex = this.content.lastIndexOf('.') + 1;
     const signature = this.content.substr(signatureStartIndex);
 

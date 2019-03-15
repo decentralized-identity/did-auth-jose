9cda2e808786ea71a822ffab6a37c40dd32ba933
Logan <logirvin@microsoft.com>
2019-03-15 11:52:38 -0700

    Added GCM support.

--- lib/crypto/aes/AesCryptoSuite.ts
+++ lib/crypto/aes/AesCryptoSuite.ts
@@ -1,4 +1,4 @@
-import CryptoSuite, {Encrypter, Signer, SymmetricEncrypter, PublicKeyConstructors} from "../../interfaces/CryptoSuite";
+import CryptoSuite, {Encrypter, Signer, SymmetricEncrypter, PublicKeyConstructors} from '../../interfaces/CryptoSuite';
 import crypto from 'crypto';
 
 export default class AesCryptoSuite implements CryptoSuite {
@@ -24,6 +24,18 @@ export default class AesCryptoSuite implements CryptoSuite {
       'A256CBC-HS512': {
         encrypt: this.encryptAesCbcHmacSha2(256, 512),
         decrypt: this.decryptAesCbcHmacSha2(256, 512)
+      },
+      'A128GCM': {
+        encrypt: this.encryptAesGcm(128),
+        decrypt: this.decryptAesGcm(128)
+      },
+      'A192GCM': {
+        encrypt: this.encryptAesGcm(192),
+        decrypt: this.decryptAesGcm(192)
+      },
+      'A256GCM': {
+        encrypt: this.encryptAesGcm(256),
+        decrypt: this.decryptAesGcm(256)
       }
     }
   }
@@ -34,7 +46,7 @@ export default class AesCryptoSuite implements CryptoSuite {
    * @param hashSize Size of the SHA2 hash
    * @returns a SymmetricEncrypter encrypt function
    */
-  private encryptAesCbcHmacSha2(keySize: number, hashSize: number): (plaintext: Buffer, additionalAuthenticatedData: Buffer) =>
+  private encryptAesCbcHmacSha2 (keySize: number, hashSize: number): (plaintext: Buffer, additionalAuthenticatedData: Buffer) =>
   Promise<{ciphertext: Buffer, initializationVector: Buffer, key: Buffer, tag: Buffer}> {
     return async (plaintext: Buffer, additionalAuthenticatedData: Buffer) => {
       const mackey = this.generateSymmetricKey(keySize);
@@ -62,7 +74,7 @@ export default class AesCryptoSuite implements CryptoSuite {
    * @param hashSize Size of the SHA2 hash
    * @returns a SymmetricEncrypter decrypt function
    */
-  private decryptAesCbcHmacSha2(keySize: number, hashSize: number):
+  private decryptAesCbcHmacSha2 (keySize: number, hashSize: number):
   (ciphertext: Buffer, additionalAuthenticatedData: Buffer, initializationVector: Buffer, key: Buffer, tag: Buffer) =>
   Promise<Buffer> {
     return async (ciphertext: Buffer, additionalAuthenticatedData: Buffer, initializationVector: Buffer, key: Buffer, tag: Buffer) {
@@ -82,6 +94,62 @@ export default class AesCryptoSuite implements CryptoSuite {
     };
   }
 
+  /**
+   * Given the encryption parameters, returns the AES GCM encryption function
+   * @param keySize Size of the keys
+   * @returns a SymmetricEncrypter encrypt function
+   */
+  private encryptAesGcm (keySize: number): (plaintext: Buffer, additionalAuthenticatedData: Buffer) =>
+  Promise<{ciphertext: Buffer, initializationVector: Buffer, key: Buffer, tag: Buffer}> {
+    return async (plaintext: Buffer, additionalAuthenticatedData: Buffer) => {
+      const key = this.generateSymmetricKey(keySize);
+      const initializationVector = this.generateInitializationVector(96);
+      const algorithm = `aes-${keySize}-gcm`;
+      const cipher = crypto.createCipheriv(algorithm, key, initializationVector) as crypto.CipherGCM;
+      cipher.setAAD(additionalAuthenticatedData);
+      const ciphertext = Buffer.concat([
+        cipher.update(plaintext),
+        cipher.final()
+      ]);
+      return {
+        ciphertext,
+        initializationVector,
+        key,
+        tag: cipher.getAuthTag()
+      };
+    };
+  }
+
+  /**
+   * Given the decryption parameters, returns an AES GCM decryption function
+   * @param keySize Size of the keys
+   * @returns a SymmetricEncrypter decrypt function
+   */
+  private decryptAesGcm (keySize: number):
+  (ciphertext: Buffer, additionalAuthenticatedData: Buffer, initializationVector: Buffer, key: Buffer, tag: Buffer) =>
+  Promise<Buffer> {
+    return async (ciphertext: Buffer, additionalAuthenticatedData: Buffer, initializationVector: Buffer, key: Buffer, tag: Buffer) {
+      const algorithm = `aes-${keySize}-gcm`;
+      const decipher = crypto.createDecipheriv(algorithm, key, initializationVector) as crypto.DecipherGCM;
+      decipher.setAAD(additionalAuthenticatedData);
+      decipher.setAuthTag(tag);
+      return Buffer.concat([
+        decipher.update(ciphertext),
+        decipher.final()
+      ]);
+    };
+  }
+
+  /**
+   * Generates the HMAC Tag
+   * @param hashSize HMAC hash size
+   * @param keySize HMAC tag size
+   * @param mackey MAC key
+   * @param additionalAuthenticatedData Additional authenticated data
+   * @param initializationVector initialization vector
+   * @param ciphertext encrypted data
+   * @returns HMAC Tag
+   */
   private generateHmacTag (hashSize: number, keySize: number, mackey: Buffer,
     additionalAuthenticatedData: Buffer, initializationVector: Buffer, ciphertext: Buffer): Buffer {
     const aadLength = additionalAuthenticatedData.length * 8;
@@ -99,11 +167,21 @@ export default class AesCryptoSuite implements CryptoSuite {
     return mac.slice(0, Math.ceil(keySize / 8));
   }
 
-  private generateSymmetricKey(bits: number): Buffer {
+  // these are two different functions to allow validation against RFC specs
+
+  /**
+   * Generates a symmetric key
+   * @param bits Size in bits of the key
+   */
+  private generateSymmetricKey (bits: number): Buffer {
     return crypto.randomBytes(Math.ceil(bits / 8));
   }
 
-  private generateInitializationVector(bits: number): Buffer {
+  /**
+   * Generates an initialization vector
+   * @param bits Size in bits of the initialization vector
+   */
+  private generateInitializationVector (bits: number): Buffer {
     return crypto.randomBytes(Math.ceil(bits / 8));
   }
 }
